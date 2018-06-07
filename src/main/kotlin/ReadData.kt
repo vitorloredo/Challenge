1783d532844b70d909b5d6c5ad7f9a57942b8230
vitorloredo <vitorloredo@hotmail.com>
2018-06-07 01:20:01 -0300

    In class ReadDate begging read data to "work" data.

--- src/main/kotlin/ReadData.kt
+++ src/main/kotlin/ReadData.kt
@@ -1,3 +1,26 @@
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
 class ReadData {
-    
+
+    private val list: MutableList<String> = mutableListOf()
+
+    fun Read() {
+
+        val baseName = File(".").canonicalPath
+        val localFileName = FileReader("$baseName\\src\\info\\base_de_respostas_10k_amostra.csv")
+        val read = BufferedReader(localFileName)
+
+        var line = read.readLine()
+        line = read.readLine()
+
+        while (line != null) {
+
+            list.add(line)
+
+            line = read.readLine()
+
+        }
+    }
 }
