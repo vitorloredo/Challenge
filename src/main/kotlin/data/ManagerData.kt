d4075c4e6dc180febbfba230f978bbfe914ba1ce
vitorloredo <vitorloredo@hotmail.com>
2018-06-07 12:04:30 -0300

    Basic ManagerData created

--- src/main/kotlin/data/ManagerData.kt
+++ src/main/kotlin/data/ManagerData.kt
@@ -1,4 +1,35 @@
 package data
 
 class ManagerData {
+    private val idCountry = hashMapOf<String, MutableList<Int>>()
+
+    fun idCountry(data: Data, value: Int) {
+        val exist = exist(data.country)
+
+        if (exist) {
+            addExist(data.country, value)
+        } else {
+            addNotExist(data.country,value)
+        }
+
+    }
+
+    private fun exist(str: String): Boolean {
+        if (idCountry.containsKey(str)) {
+            return true
+        }
+        return false
+    }
+
+    private fun addExist(str: String, value: Int) {
+        val listValue = idCountry.get(str)
+        listValue!!.add(value)
+        idCountry[str] = listValue
+    }
+
+    private fun addNotExist(str: String, value: Int) {
+        val newMutableList = mutableListOf<Int>()
+        newMutableList.add(value)
+        idCountry[str] = newMutableList
+    }
 }
